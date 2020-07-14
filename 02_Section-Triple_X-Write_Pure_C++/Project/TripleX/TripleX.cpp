89d0b0e1ce3b93bbb660d18d5b94e7ebd9ec53b6
Oleksandr <brovarsv@gmail.com>
2020-07-14 21:26:24 +0300

    Lesson 17.2
    Update TripleX.cpp

--- 02_Section-Triple_X-Write_Pure_C++/Project/TripleX/TripleX.cpp
+++ 02_Section-Triple_X-Write_Pure_C++/Project/TripleX/TripleX.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-    std::cout << "Hello, World";
+    std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
+
     return 0;
 }
