0a539ef7c42a2ad19b49a5c2965d08a495055c95
Oleksandr <brovarsv@gmail.com>
2020-07-18 14:01:03 +0300

    Lesson 25
    Using \n new line escape sequence in strings;
    Add First Function().

--- 02_Section-Triple_X-Write_Pure_C++/Project/TripleX/TripleX.cpp
+++ 02_Section-Triple_X-Write_Pure_C++/Project/TripleX/TripleX.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
     // Print welcome messages to the terminal
-    std::cout << std::endl;
-    std::cout << "============================================================";
-    std::cout << std::endl;
-    std::cout << "You are a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    std::cout << "\n============================================================";
+    std::cout << "\nYou are a secret agent breaking into a secure server room...";
+    std::cout << "\nEnter the correct code to continue...";
+}
+
+void PlayGame()
+{
+    PrintIntroduction();
 
     // Declare 3 number code
     const int CodeA = 4;
@@ -19,37 +21,38 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print CodeSum and CodeProduct to the terminal
-    std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "\n\n+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    // Declare 3 number Guess
+    // Store player guess
     int GuessA, GuessB, GuessC;
-
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     const int GuessSum = GuessA + GuessB + GuessC;
     const int GuessProduct = GuessA * GuessB * GuessC;
 
-    std::cout << std::endl;
-    std::cout << "GuessSum : " << GuessSum << std::endl;
-    std::cout << "GuessProduct : " << GuessProduct << std::endl;
+    std::cout << "\nGuessSum: " << GuessSum;
+    std::cout << "\nGuessProduct: " << GuessProduct;
 
+    // Check if the player guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << std::endl;
-        std::cout << "You win!" << std::endl;
+        std::cout << "\nYou win!";
     }
     else
     {
-        std::cout << std::endl;
-        std::cout << "You lose!";
+        std::cout << "\nYou lose!";
     }
-    
 
-    std::cout << "============================================================" << std::endl;
+    std::cout << "\nHe said: \"Hello world!\"";
+    std::cout << "\n============================================================";
+}
+
+
+
+int main()
+{
+    PlayGame();
     return 0;
 }
