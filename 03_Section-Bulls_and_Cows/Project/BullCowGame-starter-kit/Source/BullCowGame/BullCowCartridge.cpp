a2a05621c50369186e45ed7c760a431dcc563269
Oleksandr <brovarsv@gmail.com>
2020-08-07 21:52:10 +0300

    Lesson 50
    Add If(Inpet == HiddenWord)

--- 03_Section-Bulls_and_Cows/Project/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ 03_Section-Bulls_and_Cows/Project/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -6,6 +6,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Megic Number Remove!
     PrintLine(TEXT("Press enter to continue..."));
 
 }
@@ -14,5 +15,15 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 {
     ClearScreen();
     FString HiddenWord = TEXT("cake");
-    PrintLine(Input);
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have Lost!"));
+    }
+    
+    
 }
